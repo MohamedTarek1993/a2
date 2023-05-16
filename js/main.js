2227661cb8337d5db7ff191527821a1b936231ca
MohamedTarek1993 <www.mtarek789@gmail.com>
2023-05-16 17:48:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+//loader
+// let loader = document.getElementById('preloader');
+// window.addEventListener('load', function () {
+//   loader.style.display = 'none';
+// });
+//loader
+//  scroll-up 
+var btn = $('#button');
+var image = $('.navbar-light .navbar-brand img');
+var link = $('.navbar-light .nav-item .nav-link');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 100) {
+    btn.addClass('show');
+    image.css('width', '50%');
+    link.addClass('change');
+  } else {
+    btn.removeClass('show');
+    image.css('width', '70%');
+    link.removeClass('change');
+  }
+});
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
+//    scroll-up 
+// animated hamburger icon
+$(document).ready($(function () {
+  let navBtn = $('.navbar-toggler');
+  $(navBtn).click(function () {
+    $(navBtn).toggleClass('active-hamburger');
+  });
+}));
+// animated hamburger icon
+
+//=========== Active navbar Link in multi pages =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active";
+  }
+}
+//=========== Active navbar Link in multi pages =============
+
